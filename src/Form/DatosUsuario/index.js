997b3f1a277a7b6f2eefb3c1c0a4c914c390b9c3
Deiby-Zair <deybyzm@unicauca.edu.co>
2023-06-09 14:19:30 -0500

    Formulario de correo y contraseña

--- src/Form/DatosUsuario/index.js
+++ src/Form/DatosUsuario/index.js
@@ -1,63 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button, Box } from "@mui/material";
+import { validarPassword, validarEmail } from "./validaciones";
 
-class DatosUsuario extends React.Component {
+const DatosUsuario = () => {
+  const [email, setEmail] = useState({ value: "", valid: null });
+  const [password, setPassword] = useState({ value: "", valid: null });
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: {
-        value: '',
-        valid: true
-      },
-      password: {
-        value: '',
-        valid: true
-      },
-    }
-  }
-  render() {
-    return (
-      <Box
-        component="form"
-        autocomplete="off"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
+  return (
+    <Box
+      component="form"
+      autocomplete="off"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (email.valid && password.valid) {
+          console.log("Siguiente formulario");
+          console.log(email, password);
+        } else {
+          console.log("No hacer nada");
+        }
+      }}
+    >
+      <TextField
+        label="Correo electrónico"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="email"
+        error={email.valid === false}
+        helperText={email.valid === false && "Ingresa un correo electrónico válido"}
+        value={email.value}
+        onChange={(input) => {
+          const email = input.target.value;
+          const valido = validarEmail(email);
+          validarEmail(email);
+          setEmail({ value: email, valid: valido });
         }}
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log(this.state);
+      />
+      <TextField
+        label="Contraseña"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="password"
+        error={password.valid === false}
+        helperText={password.valid === false && "Ingresa una contraseña válida, Al menos 8 caracteres y máximo 20"}
+        value={password.value}
+        onChange={(input) => {
+          const password = input.target.value;
+          setPassword({ value: password, valid: validarPassword(password) })
         }}
-      >
-        <TextField
-          label="Correo electrónico"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-          type="email"
-          error={false}
-          helperText={false && "Ingresa un correo electrónico válido"}
-          value={this.state.email.value}
-          onChange={(input) => this.setState({ email: { value: input.target.value } })}
-        />
-        <TextField
-          label="Contraseña"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-          type="password"
-          value={this.state.password.value}
-          onChange={(input) => this.setState({ password: { value: input.target.value } })}
-        />
-        <Button variant="contained" type="submit">
-          Siguiente
-        </Button>
-      </Box>
-    );
-  }
+      />
+      <Button variant="contained" type="submit">
+        Siguiente
+      </Button>
+    </Box>
+  );
+
 }
 
 export default DatosUsuario;
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Typography } from "@mui/material";
 import { LogoSpace, FormSpace, Img } from "./styles";
 import DatosUsuario from "./DatosUsuario";
