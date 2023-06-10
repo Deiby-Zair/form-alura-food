16e910e56f2375a4e7f71b689c58bb712a8f348b
Deiby-Zair <deybyzm@unicauca.edu.co>
2023-06-09 20:30:07 -0500

    Optimización de código

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { LogoSpace, FormSpace, Img } from "./styles";
 import DatosUsuario from "./DatosUsuario";
@@ -7,8 +7,34 @@ import DatosEntrega from "./DatosEntrega";
 import Complete from "./Complete";
 import Stepper from "../Stepper";
 
+//Validaciones
+import { validarEmail, validarPassword } from "./DatosUsuario/validaciones";
+import Step from "./Step";
+
 const Form = () => {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(0);
+  const [pasos, setPasos] = useState([]);
+
+
+  useEffect(() => {
+    console.log("Use effect");
+  });
+
+  useEffect(() => {
+    console.log("Se ha actualizado el step");
+  }, [step])
+
+  // useEffect(async () => {
+  //   try {
+  //     const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //     const posts = await data.json();
+  //     console.log(posts);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }, [])
+
+  console.log("Form Componente");
 
   //step = 0 --> <DatosUsuario />
   //step = 1 --> <DatosPersonales />
@@ -27,6 +53,78 @@ const Form = () => {
     3: <Complete />
   }
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let newStep = step + 1;
+    setStep(newStep);
+    console.log("new step", newStep);
+    console.log(step);
+  }
+
+  const handleChange = (element, position, currentStep, validator) => {
+    const value = element.target.value;
+    const valid = validator(value);
+    console.log(value);
+    console.log(valid);
+    console.log("position", position);
+    console.log(currentStep);
+    console.log(validator);
+
+    stepsFlow[0].inputs[0].label = "Maiahahah";
+    console.log(stepsFlow);
+  };
+
+  const stepsFlow = {
+    0: {
+      inputs: [
+        {
+          label: "Correo electrónico",
+          type: "email",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa un correo electrónico válido",
+          validator: validarEmail,
+        },
+        {
+          label: "Contraseña",
+          type: "password",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa una contraseña válida",
+          validator: validarPassword,
+        },
+      ],
+      buttonText: "Siguiente",
+      onSubmit
+    },
+    1: {
+      inputs: [
+        {
+          label: "Correo electrónico",
+          type: "email",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa un correo electrónico válido",
+          validator: validarEmail,
+        },
+        {
+          label: "Contraseña",
+          type: "password",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa una contraseña válida",
+          validator: validarPassword,
+        },
+      ],
+      buttonText: "Siguiente",
+      onSubmit
+    }
+  }
+
   return (
     <Box
       sx={{
@@ -41,10 +139,10 @@ const Form = () => {
       </LogoSpace>
       <FormSpace>
         {step < 3 && < Stepper step={step} />}
-        {/* <DatosUsuario /> */}
-        {/* <DatosPersonales />
-        <DatosEntrega /> */}
-        {steps[step]}
+
+        {/* {steps[step]} */}
+        <Step data={stepsFlow[step]} step={step} />
+
       </FormSpace>
     </Box>
   );
