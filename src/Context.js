16660cfc0567b50377260bb9bf2c118ccff6630b
Deiby-Zair <deybyzm@unicauca.edu.co>
2023-06-09 21:03:22 -0500

    Contexto y providers

---
+++ src/Context.js
@@ -0,0 +1,23 @@
+import React, { useState, createContext } from "react";
+
+export const CounterContext = createContext();
+
+export const CounterProvider = ({ children }) => {
+    const [count, setCount] = useState(0);
+    const values = {
+        count,
+        suma() {
+            setCount(val => val + 1)
+        },
+        resta() {
+            setCount(val => val - 1)
+        },
+        user: {
+            status: "Online",
+            jwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJzdGF0dXMiOiJvbmxpbmUifQ.lCg6KWiP0-MKaAbusLwDCqOxfYiumSKeLXmKI13m4nc",
+            theme: "light",
+        },
+    };
+
+    return <CounterContext.Provider value={values}>{children}</CounterContext.Provider>
+};
